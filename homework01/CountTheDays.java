07b3b6f21c3cec5e65d1a22ad61723d779f6e7ea
Veda-Ashok <35431161+Veda-Ashok@users.noreply.github.com>
2018-01-22 21:35:02 -0800

    Create CountTheDays.java

---
+++ homework01/CountTheDays.java
@@ -0,0 +1,16 @@
+public class CountTheDays{
+	public static void main(String[] args){
+	   Scanner m = new Scanner(System.in); // user input for month
+	   Scanner d = new Scanner(System.in); // user input for day
+	   Scanner y = new Scanner(System.in); // user input for year
+
+	   System.out.print("Please enter a value that corresponds to a month: "); // prompt user to enter month
+	   long month = m.nextLong();
+	   System.out.print("Please enter a value that corresponds to a day: "); // prompt user to enter day
+	   long day = d.nextLong();
+	   System.out.print("Please enter a value that corresponds to a year: "); //prompt user to enter year
+	   long year = y.nextLong();
+
+	   
+	}
+}
