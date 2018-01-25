1982e789289cb1063d0b37daf166ed92f45725df
Veda-Ashok <35431161+Veda-Ashok@users.noreply.github.com>
2018-01-24 19:25:57 -0800

    Update CountTheDays.java

--- homework01/CountTheDays.java
+++ homework01/CountTheDays.java
@@ -1,22 +1,11 @@
 /**
  *  File name     :  CountTheDays.java
- *  Purpose       :  Provides a class with supporting methods for CountTheDays.java program
- *  Author        :  B.J. Johnson (prototype)
- *  Date          :  2017-01-02 (prototype)
+ *  Purpose       :  CountTheDays.java program
  *  Author        :  <Veda Ashok>
  *  Date          :  <18 January 2018>
  *  Description   :  This program calculates the number of days between two dates. The dates are specified 
  *					 as args[0] through args[5].
- *  Notes         :  None
- *  Warnings      :  None
- *  Exceptions    :  None
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- *  Revision History
- *  ----------------
- *            Rev      Date     Modified by:  Reason for change/modification
- *           -----  ----------  ------------  -----------------------------------------------------------
- *  @version 1.0.0  2017-01-02  B.J. Johnson  Initial writing and release
- *  @version 1.0.1  2017-12-25  B.J. Johnson  Updated for Spring 2018
  */
 
 public class CountTheDays{
