1a9dfa6b1a36baea31c5bb2e6410579c3f703c9a
Veda-Ashok <35431161+Veda-Ashok@users.noreply.github.com>
2018-01-24 18:59:57 -0800

    Update CountTheDays.java

--- homework01/CountTheDays.java
+++ homework01/CountTheDays.java
@@ -1,29 +1,33 @@
+/**
+ *  File name     :  CalendarStuff.java
+ *  Purpose       :  Provides a class with supporting methods for CountTheDays.java program
+ *  Author        :  B.J. Johnson (prototype)
+ *  Date          :  2017-01-02 (prototype)
+ *  Author        :  <Veda Ashok>
+ *  Date          :  <18 January 2018>
+ *  Description   :  This program calculates the number of days between two dates. The dates are specified 
+ *					 as args[0] through args[5].
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History
+ *  ----------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2017-01-02  B.J. Johnson  Initial writing and release
+ *  @version 1.0.1  2017-12-25  B.J. Johnson  Updated for Spring 2018
+ */
+
 public class CountTheDays{
 	public static void main(String[] args){
-	   Scanner m1 = new Scanner(System.in); // user input for month
-	   Scanner d1 = new Scanner(System.in); // user input for day
-	   Scanner y1 = new Scanner(System.in); // user input for year
-	   Scanner m2 = new Scanner(System.in); // second user input for month
-	   Scanner d2 = new Scanner(System.in); // second user input for day
-	   Scanner y2 = new Scanner(System.in); // second user input for year
+	   long[] dateStuff = new long[6];
+	   for(int i = 0; i < 6; i++){
+	   	dateStuff[i] = Long.parseLong(args[i]);
 
-	   System.out.print("Please enter a value that corresponds to a month: "); // prompt user to enter first month
-	   long month = m1.nextLong();
-	   System.out.print("Please enter a value that corresponds to a day: "); // prompt user to enter first day
-	   long day = d1.nextLong();
-	   System.out.print("Please enter a value that corresponds to a year: "); //prompt user to enter first year
-	   long year = y1.nextLong();
-
-	    System.out.print("Please enter another value that corresponds to a month: "); // prompt user to enter second month
-	   long month = m2.nextLong();
-	   System.out.print("Please enter another value that corresponds to a day: "); // prompt user to enter second day
-	   long day = d2.nextLong();
-	   System.out.print("Please enter another value that corresponds to a year: "); //prompt user to enter second year
-	   long year = y2.nextLong();
-
-	   CalendarStuff.daysBetween(m1 , d1, y1, m2, d2, y2);
-
-
+	   }
+	   
+	   	System.out.println("The number of days between the two dates is:" + CalendarStuff.daysBetween(dateStuff[0], dateStuff[1], dateStuff[2], dateStuff[3], dateStuff[4], dateStuff[5]));
 
 	}
 }
