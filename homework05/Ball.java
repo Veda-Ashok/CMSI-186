caf0cccece6a96b9087dd27ff829ba3fed2dfcfe
Veda-Ashok <35431161+Veda-Ashok@users.noreply.github.com>
2018-03-26 17:55:10 -0700

    Update Ball.java

--- homework05/Ball.java
+++ homework05/Ball.java
@@ -1,3 +1,15 @@
+/**
+* This program simulates a soccer ball with a radius of 4.45 inches. It calculates the velocity of the soccer ball after it is lauched at the requested
+* velocity. It takes into accound friction.
+*
+*
+* @Author Veda Ashok
+* @Date 20 March 2018
+*
+*
+*
+*/
+
 import java.lang.Math;
 import java.text.DecimalFormat;
 
@@ -10,13 +22,10 @@ public class Ball{
 	public	double initialYSpeed = 0;
 	public double timeSlice =0;
 	public double velocity = 0;
-	 
-	// public double movingPosX = 0;
-	// public double movingPosY = 0;
+	
 
 
-
-	public Ball(double x, double y, double xv, double yv, double ts){
+	public Ball(double x, double y, double xv, double yv, double ts){ //constructor for ball
 		xPosition = x;
 		yPosition = y;
 		initialXSpeed = xv;
@@ -27,23 +36,7 @@ public class Ball{
 
 	
 
-	public void convertValues(String xArg, String yArg, String xSpeedArg, String ySpeedArg){
-
-		try {
-			xPosition = Double.parseDouble(xArg);
-			yPosition = Double.parseDouble(yArg);
-			initialXSpeed = Double.parseDouble(xSpeedArg);
-			initialYSpeed = Double.parseDouble(ySpeedArg);
-		}
-		catch (NumberFormatException nfe){
-			System.out.println("Invalid argument. Please try again.");
-			System.exit(1);
-		}
-
-
-	}
-
-	public double[] move(){
+	public double[] move(){  //calculates new positions and speeds as ball moves
 
 		
 		xPosition += initialXSpeed;
@@ -69,12 +62,8 @@ public class Ball{
 		
 	}
 
-/*
-* This method is intended to calculate the velocity of the ball with friction as a factor
-*
-*
-*/
-	public  double velocity(){
+
+	public double velocity(){    //uses hypotenuse as overall velocity
 
 		
 
@@ -90,25 +79,26 @@ public class Ball{
 	}
 
 
-	public String toString(){
+	public String toString(){   //displays results of soccer balls
 
 		DecimalFormat xPos = new DecimalFormat("#0.00");
 		DecimalFormat yPos = new DecimalFormat("#0.00");
 		DecimalFormat xVel = new DecimalFormat("#0.000");
 		DecimalFormat yVel = new DecimalFormat("#0.000");
 
-		if(velocity <= (1.0/12.0)){
-			return ("<" + xPos.format(xPosition) + " , " + yPos.format(yPosition) + " > " +
-		       "< At Rest > ");
-			
-		}
-		{
-
-			
+		if(velocity > (1.0/12.0)){ 
 			return ("<" + xPos.format(xPosition) + " , " + yPos.format(yPosition) + " > " +
 		       "<" + xVel.format(initialXSpeed) + " , " + yVel.format(initialYSpeed) + " > ");
-
+			
 		}
+
+		else if(velocity <= (1.0/12.0)){
+		
+			return ("<" + xPos.format(xPosition) + " , " + yPos.format(yPosition) + " > " +
+		       "< At Rest > ");
+		}
+
+		return "";
 
 
 		
@@ -117,15 +107,21 @@ public class Ball{
 
 	public static void main(String []args){
 		
-		Ball b = new Ball(4.0,5.0,6.0,7.0, 1.0);
-		b.toString();
-		b.velocity();
+		Ball test1 = new Ball(4.0,5.0,6.0,7.0, 1.0);
+		Ball test2 = new Ball(20, 80, 700, 900, 7.0);
+	
 
       System.out.println( "    Testing velocity");
-      System.out.print( "      sending '  6.0 and 7.0', expecting double value   9.2195" );
-      try { System.out.println( ((Math.sqrt(85) * Math.pow(0.99, 1.0))== b.velocity()) ? " - got 9.2195" : " - no" ); }
+      System.out.print( "      sending '  6.0 and 7.0', expecting double value   9.1273" );
+      try { System.out.println( ((Math.sqrt(85) * Math.pow(0.99, 1.0))== test1.velocity()) ? " - got it" : " - no" ); }
       catch( Exception e ) { System.out.println ( " - Exception thrown: " + e.toString() ); }
-
+      System.out.println( "    Testing velocity");
+      System.out.print( "      sending '  700 and 900', expecting double value   1062.7180" );
+      try { System.out.println( ((Math.sqrt(1300000) * Math.pow(0.99, 7.0))== test2.velocity()) ? " - got it" : " - no" ); }
+      catch( Exception e ) { System.out.println ( " - Exception thrown: " + e.toString() ); }
+       
 
 	}
+
+
 }
